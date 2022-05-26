aeb4d6ff9a5c05500c6f01a2733216454ef0f153
matthew.she <matthew.she@mail.utoronto.ca>
2022-05-26 15:45:24 -0400

    update submodule

--- src/calculate_lambda.cpp
+++ src/calculate_lambda.cpp
@@ -8,8 +8,12 @@ void calculate_lambda(
     Eigen::VectorXd lambda){
     //C_i = rho_i/rho_0 -1
     //rho_i = Sigma m_j * W(p_i-p_j,h)
+    double c;
+    double grad_c
     for (int i = 0; i < x.rows(); i++){
-        C(x, N, rho_0, i, h);
+        c = C(x, N, rho_0, i, h);
+        grad_c = grad_C_squared(x, N, rho_0, i, h);
+        lamda(i) = -c/grad_c
     }
     
 }
@@ -36,4 +40,26 @@ double C(const Eigen::MatrixXd x,
         }
     }
     return rho_i/rho_0 - 1.0;
+}
+
+double grad_C_squared(const Eigen::MatrixXd x, 
+    const Eigen::MatrixXd N,
+    const double rho_0;
+    const int i,
+    const double h){
+    double sum_k = 0;
+    for (int k = 0; k < x.rows(); k++){
+        Eigen::Vector3d sum_j;
+        if (k == i){
+            for (int j = 0; j < x.rows(); j++){
+                sum_j += 45 * pow(h - (x(i) -x(j).norm), 2) / (3.14 * pow(h, 6)) * x(k);
+            }
+            sum_j = sum_j/rho_0;
+            sum_k += pow(sum_j.norm, 2);
+        }
+        else{
+            sum_k += pow((-45 * pow(h - (x(i) -x(k).norm), 2) / (3.14 * rho_0 * pow(h, 6)) * x(k)).norm, 2);
+        }
+    }
+    return sum_k;
 }
