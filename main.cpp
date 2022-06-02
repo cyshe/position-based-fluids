d59ce75d31810cf8fed662d44b161d9089888066
matthew.she <matthew.she@mail.utoronto.ca>
2022-06-02 10:57:42 -0400

    fixed compilation, added missing files from previous 2 commits

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include<iostream>
-#include<Eigen/Core>
+#include <igl/opengl/glfw/Viewer.h>
+
+//#include<Eigen/Core>
 
 int main(int argc, char *argv){
 
-    //input list of positions
-    Eigen::MatrixXd v;
-    Eigen::MatrixXd x;
-    Eigen::MatrixXd f_ext;
-    Eigen::MatrixXd N;
-    double dt = 0.001;
-    
-    
-    predict_position(x, v, f_ext, dt);
-    find_neighbor(x,N);
-    forces();
-    energy_refinement();
-    update_velocity();
+    // Inline mesh of a cube
+  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
+    0.0,0.0,0.0,
+    0.0,0.0,1.0,
+    0.0,1.0,0.0,
+    0.0,1.0,1.0,
+    1.0,0.0,0.0,
+    1.0,0.0,1.0,
+    1.0,1.0,0.0,
+    1.0,1.0,1.0).finished();
 
-    return 0;
+  const Eigen::MatrixXd C = (Eigen::MatrixXd(1,3) << 0, 0, 1.0).finished();
+  // Plot the mesh
+  igl::opengl::glfw::Viewer viewer;
+  viewer.data().set_points(V, C);
+  viewer.launch();
+
+    //input list of positions
+//    Eigen::MatrixXd v;
+  //  Eigen::MatrixXd x;
+    //Eigen::MatrixXd f_ext;
+//    Eigen::MatrixXd N;
+  //  double dt = 0.001;
+    
+    
+    //predict_position(x, v, f_ext, dt);
+   // find_neighbor(x,N);
+    //forces();
+    //energy_refinement();
+    //update_velocity();
+
+    //return 0;
 }
